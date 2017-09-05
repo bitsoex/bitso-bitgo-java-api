721b55be97ce0e8431a22b181cc40bfeeb6b64f9
Enrique Zamudio <alterbass@gmail.com>
2017-09-05 10:16:33 -0500

    simpler

--- bitgo-java-api/src/main/java/com/bitso/bitgo/SendCoinsResponse.java
+++ bitgo-java-api/src/main/java/com/bitso/bitgo/SendCoinsResponse.java
@@ -1,7 +1,6 @@
 package com.bitso.bitgo;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import java.math.BigDecimal;
 
@@ -11,20 +10,14 @@ import java.math.BigDecimal;
  * @author Enrique Zamudio
  *         Date: 5/8/17 4:51 PM
  */
+@Data
 public class SendCoinsResponse extends AbstractResponse {
 
-    @Setter @Getter
     private String tx;
-    @Setter @Getter
     private String hash;
-    @Setter @Getter
     private BigDecimal fee;
-    @Setter @Getter
     private BigDecimal feeRate;
-    @Setter @Getter
     private String pendingApproval;
-    @Setter @Getter
     private boolean otp;
-    @Setter @Getter
     private String triggeredPolicy;
 }
