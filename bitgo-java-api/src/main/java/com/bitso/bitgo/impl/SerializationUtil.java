02b2f22dfeb888278cdb8834d9c463d962385046
kushal <kushal@bitso.com>
2018-08-16 15:38:38 -0700

    cleanup JSON handling

---
+++ bitgo-java-api/src/main/java/com/bitso/bitgo/impl/SerializationUtil.java
@@ -0,0 +1,13 @@
+package com.bitso.bitgo.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * @author kushal256
+ * Date: 8/16/18
+ */
+public class SerializationUtil {
+
+
+    public static final ObjectMapper mapper = new ObjectMapper();
+}
