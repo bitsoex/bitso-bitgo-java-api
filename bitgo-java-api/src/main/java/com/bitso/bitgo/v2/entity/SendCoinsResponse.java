33f666c651c9fcac45d04a56db6dd4a4260951b8
kushal <kushal@bitso.com>
2018-09-26 14:13:15 -0700

    Fix build warning, change lombok dependency to remove gradle deprecation warning

--- bitgo-java-api/src/main/java/com/bitso/bitgo/v2/entity/SendCoinsResponse.java
+++ bitgo-java-api/src/main/java/com/bitso/bitgo/v2/entity/SendCoinsResponse.java
@@ -1,6 +1,7 @@
 package com.bitso.bitgo.v2.entity;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * A response to the SendCoins/SendCoinsToMultipleAddresses request.
@@ -9,6 +10,7 @@ import lombok.Data;
  * Date: 5/8/17 4:51 PM
  */
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class SendCoinsResponse extends AbstractResponse {
 
     private String tx;    //hex-encoded tx
